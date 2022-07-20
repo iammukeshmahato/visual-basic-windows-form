f904bd808dbd25fb9e44abff67eb650d608e48a9
Mukesh Mahato <55306415+iammukeshmahato@users.noreply.github.com>
2022-07-20 15:24:34 +0545

    Add files via upload

---
+++ DaysToYearConverter/DaysToYearConverter/Form1.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.inputBox = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.year = New System.Windows.Forms.TextBox()
+        Me.months = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.days = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(149, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(223, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Days To Year Converter"
+        '
+        'inputBox
+        '
+        Me.inputBox.Location = New System.Drawing.Point(266, 109)
+        Me.inputBox.Name = "inputBox"
+        Me.inputBox.Size = New System.Drawing.Size(100, 20)
+        Me.inputBox.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(149, 104)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(111, 24)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "No of days:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(231, 156)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(153, 217)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Year"
+        '
+        'year
+        '
+        Me.year.Location = New System.Drawing.Point(188, 217)
+        Me.year.Name = "year"
+        Me.year.Size = New System.Drawing.Size(50, 20)
+        Me.year.TabIndex = 5
+        '
+        'months
+        '
+        Me.months.Location = New System.Drawing.Point(316, 217)
+        Me.months.Name = "months"
+        Me.months.Size = New System.Drawing.Size(50, 20)
+        Me.months.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(269, 220)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "months"
+        '
+        'days
+        '
+        Me.days.Location = New System.Drawing.Point(452, 217)
+        Me.days.Name = "days"
+        Me.days.Size = New System.Drawing.Size(50, 20)
+        Me.days.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(417, 217)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(29, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "days"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(576, 512)
+        Me.Controls.Add(Me.days)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.months)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.year)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.inputBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents inputBox As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents year As TextBox
+    Friend WithEvents months As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents days As TextBox
+    Friend WithEvents Label5 As Label
+End Class
---
+++ DaysToYearConverter/DaysToYearConverter/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim inputDays As Integer
+        inputDays = inputBox.Text
+
+        year.Text = Int(inputDays / 365)
+        months.Text = Int((inputDays Mod 365) / 30)
+        days.Text = (inputDays Mod 365) Mod 30
+    End Sub
+End Class
