48fb225aa6fa8327c758b0a84e9a507a3a3833f3
Mukesh Mahato <55306415+iammukeshmahato@users.noreply.github.com>
2022-08-04 14:19:27 +0545

    Add files via upload

---
+++ dialoge Box/dialoge Box/Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.principle = New System.Windows.Forms.TextBox()
+        Me.time = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.rate = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.calculate = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(80, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(227, 23)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Simple Interest Calculator"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(86, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 19)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Principle"
+        '
+        'principle
+        '
+        Me.principle.Location = New System.Drawing.Point(153, 77)
+        Me.principle.Name = "principle"
+        Me.principle.Size = New System.Drawing.Size(100, 20)
+        Me.principle.TabIndex = 2
+        '
+        'time
+        '
+        Me.time.Location = New System.Drawing.Point(153, 122)
+        Me.time.Name = "time"
+        Me.time.Size = New System.Drawing.Size(100, 20)
+        Me.time.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(86, 121)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 19)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Time"
+        '
+        'rate
+        '
+        Me.rate.Location = New System.Drawing.Point(153, 159)
+        Me.rate.Name = "rate"
+        Me.rate.Size = New System.Drawing.Size(100, 20)
+        Me.rate.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(86, 158)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(37, 19)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Rate"
+        '
+        'calculate
+        '
+        Me.calculate.Location = New System.Drawing.Point(162, 206)
+        Me.calculate.Name = "calculate"
+        Me.calculate.Size = New System.Drawing.Size(75, 23)
+        Me.calculate.TabIndex = 7
+        Me.calculate.Text = "Calculate"
+        Me.calculate.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(412, 261)
+        Me.Controls.Add(Me.calculate)
+        Me.Controls.Add(Me.rate)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.time)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.principle)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents principle As TextBox
+    Friend WithEvents time As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents rate As TextBox
+    Friend WithEvents Label4 As Label
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Friend WithEvents calculate As Button
+
+
+End Class
---
+++ dialoge Box/dialoge Box/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub calculate_Click(sender As Object, e As EventArgs) Handles calculate.Click
+        Dim si As Double
+        si = (principle.Text * time.Text * rate.Text) / 100
+        MessageBox.Show("The simple interest = " & si)
+
+
+    End Sub
+End Class
